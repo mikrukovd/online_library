2f8451c9515924c0b84d451b4800d1dcec0a0ab9
mikrukovd <z123456689z@gmail.com>
2025-08-13 11:34:08 +0300

    Split books by two columns.

--- render_website.py
+++ render_website.py
@@ -2,6 +2,7 @@ import json
 
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from more_itertools import chunked
 
 
 def render_page():
@@ -12,8 +13,13 @@ def render_page():
         loader=FileSystemLoader("."),
         autoescape=select_autoescape(["html"])
     )
+    env.filters["chunked"] = chunked
     template = env.get_template("template.html")
-    page = template.render(books=books)
+
+    chunk_size = (len(books)+1) // 2
+    columns = list(chunked(books, chunk_size))
+
+    page = template.render(columns=columns)
 
     with open("index.html", "w", encoding="utf8") as file:
         file.write(page)
@@ -24,4 +30,4 @@ render_page()
 server = Server()
 server.watch("template.html", render_page)
 
-server.serve(root=".", port=5500, host="0.0.0.0")
+server.serve(root=".", port=5500, host="127.0.0.1")
